a6f98c9d6e195b3882a992cf60fbd00f8d4107bb
PK Banks <pkbanks12@gmail.com>
2017-12-27 16:46:27 -0500

    added comments, removed unused code'

--- app.js
+++ app.js
@@ -3,16 +3,10 @@ const app = express()
 
 app.use(express.static('public'))
 
+// prints hello world as response
 // app.get('/', (req, res) => res.send('Hello World!'))
 
+// routes to hello.html in public directory
 app.get('/', (req, res) => res.sendFile('hello.html', {root: 'public'}))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
-
-app.post('/', function(req, res){
-  res.send('got a POST request');
-})
-
-app.put('/user', function(req, res){
-  res.send('got a PUT request at /user');
-})
