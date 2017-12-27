7a61d5bd4aae3fb140b1f4a33da0253cbf3005b0
PK Banks <pkbanks12@gmail.com>
2017-12-27 16:45:25 -0500

    played with routes, mapped root to public/hello.html

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static('public'))
+
+// app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/', (req, res) => res.sendFile('hello.html', {root: 'public'}))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
+app.post('/', function(req, res){
+  res.send('got a POST request');
+})
+
+app.put('/user', function(req, res){
+  res.send('got a PUT request at /user');
+})
